9d0000e6f83e57b587d1de384a5d6a1c912acd35
Samuel Sisay <samuel.sisay33@gmail.com>
2020-12-15 17:59:41 +0300

    separated map

--- final_ui.py
+++ final_ui.py
@@ -20,7 +20,6 @@ import pickle
 import threading
 import ui.utilities as utilities
 import numpy as np
-import time
 import queue
 import cv2
 
@@ -28,11 +27,10 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QPushButton
 from PyQt5.QtGui import QPixmap, QImage
-import numpy as np
+
 import sys
-from time import sleep
 
-
+from ui.map2d import *
 
 from client_data import *
 
@@ -1060,80 +1058,6 @@ class Ui_Form(QtWidgets.QWidget):
 	def export(self):
 		path = QtWidgets.QFileDialog.getOpenFileName(self, 'Save Model', ClientData.modelValues.exportPath, "OBJ files (*.obj)")
 		ClientData.triggers.exportModelTrigger = True
-
-
-
-class OwnImageWidget(QtWidgets.QWidget):
-		def __init__(self, parent=None):
-			super(OwnImageWidget, self).__init__(parent)
-			self.image = None
-
-		def setImage(self, image):
-			self.image = image
-			sz = image.size()
-			self.setMinimumSize(sz)
-			self.update()
-
-		def paintEvent(self, event):
-			qp = QtGui.QPainter()
-			qp.begin(self)
-			if self.image:
-				qp.drawImage(QtCore.QPoint(0, 0), self.image)
-			qp.end()
-
-
-class UpdateView(QtCore.QThread):
-
-	updated = QtCore.pyqtSignal(object)
-
-	def __init__(self, parent=None):
-		super(UpdateView, self).__init__(parent)
-		self.img= np.zeros((500, 500), dtype=np.uint8)
-		
-
-	def run(self):
-		
-		obstacleColor=np.array([255,255,255])
-		empty=np.array([0,0,0])
-		droneColor=np.array([255,0,0])
-		imageColored=np.zeros((500, 500,3), dtype=np.uint8)
-		while True:
-			#print(imageColored[255][10])
-			if self.img[255][10]==1:
-				self.img = np.zeros((500, 500), dtype=np.uint8)
-				imageColored=np.zeros((500, 500,3), dtype=np.uint8)
-			else:
-				self.img[250:499, 0:100] = 1
-				imageColored[self.img==1]=obstacleColor
-				
-				#imageColored[250:429, 10:80]=[255,0,0]
-			self.updated.emit(imageColored)
-			sleep(1)
-
-
-class MapView(QWidget):
-	def __init__(self):
-		super(MapView, self).__init__()
-		self.label = QLabel()
-		
-		self.updateView=UpdateView()
-		img = np.zeros((500, 500,3), dtype=np.uint8)
-		img[100:200,100:200]=[255,0,0]
-
-		pin = QPixmap(QImage(img.data, img.shape[0], img.shape[1], QImage.Format_RGB888))
-		self.label.setPixmap(pin)
-		layout = QVBoxLayout()
-		layout.addWidget(self.label)
-		self.setLayout(layout)
-		self.loadStuff()
-	def loadStuff(self):
-		self.updateView.updated.connect(self.on_data_ready)
-		self.updateView.start()
-	def on_data_ready(self, data):
-		print(data[255][10])
-		qImg = QPixmap(QImage(data.data, data.shape[0], data.shape[1], QImage.Format_RGB888))
-		self.label.setPixmap(qImg)
-		self.label.update()
 
 
 if __name__ == "__main__":
--- map2d.py
+++ map2d.py
@@ -1,14 +1,95 @@
-import pyglet
-from pyglet.window import mouse
+if __name__ == '__main__':
 
-window = pyglet.window.Window()
+	import os
+	import sys
 
-@window.event
-def on_draw():
-    window.clear()
+	mainDirectory = os.getcwd()
 
-@window.event
-def on_mouse_drag(x, y, dx, dy, buttons, modifiers):
-    pyglet.graphics.draw(4, pyglet.gl.GL_QUADS, ('v2f', [x, y, dx, y, dx, dy, x, dy]))
+	sys.path.append(mainDirectory + '\\..')
 
-pyglet.app.run()
+	os.chdir(mainDirectory + '\\..')
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QPushButton
+from PyQt5.QtGui import QPixmap, QImage
+import numpy as np
+import time
+
+from client_data import *
+
+import pynput
+import keyboard
+
+obstacleColor = np.array([255,255,255])
+emptyColor = np.array([0,0,0])
+droneColor = np.array([255,0,0])
+
+class OwnImageWidget(QtWidgets.QWidget):
+		def __init__(self, parent=None):
+			super(OwnImageWidget, self).__init__(parent)
+			self.image = None
+
+		def setImage(self, image):
+			self.image = image
+			sz = image.size()
+			self.setMinimumSize(sz)
+			self.update()
+
+		def paintEvent(self, event):
+			qp = QtGui.QPainter()
+			qp.begin(self)
+			if self.image:
+				qp.drawImage(QtCore.QPoint(0, 0), self.image)
+			qp.end()
+
+
+class UpdateView(QtCore.QThread):
+
+	updated = QtCore.pyqtSignal(object)
+
+	def __init__(self, parent=None):
+		super(UpdateView, self).__init__(parent)
+		self.img = ClientData.vehicleInformation.locationBitmap
+		
+
+	def run(self):
+		
+		imageColored=np.zeros((500, 500,3), dtype=np.uint8)
+		self.img = ClientData.vehicleInformation.locationBitmap
+
+		while True:
+			#print(imageColored[255][10])
+			imageColored[self.img==1] = obstacleColor
+			imageColored[self.img==2] = droneColor
+			
+			#imageColored[250:429, 10:80]=[255,0,0]
+			self.updated.emit(imageColored)
+			time.sleep(1)
+
+
+class MapView(QWidget):
+
+	def __init__(self):
+		super(MapView, self).__init__()
+		self.label = QLabel()
+		
+		self.updateView = UpdateView()
+		img = np.zeros((500, 500,3), dtype=np.uint8)
+		img[100:200,100:200]=[255,0,0]
+
+		pin = QPixmap(QImage(img.data, img.shape[0], img.shape[1], QImage.Format_RGB888))
+		self.label.setPixmap(pin)
+		layout = QVBoxLayout()
+		layout.addWidget(self.label)
+		self.setLayout(layout)
+		self.loadStuff()
+
+	def loadStuff(self):
+		self.updateView.updated.connect(self.on_data_ready)
+		self.updateView.start()
+	
+	def on_data_ready(self, data):
+		qImg = QPixmap(QImage(data.data, data.shape[0], data.shape[1], QImage.Format_RGB888))
+		self.label.setPixmap(qImg)
+		self.label.update()
